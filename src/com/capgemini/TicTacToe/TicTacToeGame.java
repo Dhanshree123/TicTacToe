79bdf9bc13d53cc583274f87e92864d94cbcda5f
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-05 09:44:56 +0530

    Add UC2

--- src/com/capgemini/TicTacToe/TicTacToeGame.java
+++ src/com/capgemini/TicTacToe/TicTacToeGame.java
@@ -1,13 +1,30 @@
 package com.capgemini.TicTacToe;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToeGame {
-
 	
+
 	public static char[] createBoard() {
 		char board[] = new char[10];
-		Arrays.fill(board,' ');
+		Arrays.fill(board, ' ');
 		return board;
+	}
+
+	public static char[] userInput() {
+		// TODO Auto-generated method stub
+		char[] ch = new char[2];
+		Scanner sc = new Scanner(System.in);
+		ch[0] = sc.next().charAt(0);
+		ch[1] = ' ';
+		if(ch[0] == 'X')
+			ch[1] = 'O';
+		
+		else
+			ch[1] = 'X';
+		
+		return ch;
+		
 	}
 }
--- src/com/capgemini/TicTacToe/TicTacToeMain.java
+++ src/com/capgemini/TicTacToe/TicTacToeMain.java
@@ -1,10 +1,17 @@
 package com.capgemini.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToeMain {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to game");
-		
 		char board[] = TicTacToeGame.createBoard();
+		System.out.println("User enter the input");
+		char inputs[] = TicTacToeGame.userInput();
+		System.out.println("User input "+inputs[0]);
+		System.out.println("Computer input "+inputs[1]);
+		
 	}
 }
