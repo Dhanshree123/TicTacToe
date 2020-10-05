fef74326f6845e7b82a5dd1fa39c79fba0d2e64b
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-05 11:07:32 +0530

    UC3 Refactor

--- src/com/capgemini/TicTacToe/TicTacToeGame.java
+++ src/com/capgemini/TicTacToe/TicTacToeGame.java
@@ -1,42 +1,40 @@
 package com.capgemini.ticTacToe;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
 	public static final Scanner SC = new Scanner(System.in);
-
 	public static char b[][] = new char[3][3];
-	
+
 	private static void fillBoard() {
-		for(int i =0 ;i<3; i++)
-			for(int j=0;j<3;j++)
-				b[i][j] ='X';
-	}
-	
-	public static char[] createBoard() {
-		char board[] = new char[10];
-		Arrays.fill(board, ' ');
-		return board;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				b[i][j] = 'X';
 	}
 
+	
+	//UC 1 - Create Board
+	public static char[][] createBoard() {
+		fillBoard();
+		return b;
+	}
+
+	//UC 2 - Check user and computer inputs
 	public static char[] userInput() {
 		char[] ch = new char[2];
 		ch[0] = SC.next().charAt(0);
 		ch[1] = ' ';
 		ch[1] = ch[0] == 'X' ? 'O' : 'X';
-		SC.close();
 		return ch;
 
 	}
-
+	
+   //UC 3 - Show Board
 	public static void showBoard() {
-		fillBoard();
-		System.out.print(b[0][0] + " | "+ b[0][1]+" | "+b[0][2]);
-		System.out.println();
-		System.out.print(b[1][0] + " | "+ b[1][1]+" | "+b[1][2]);
-		System.out.println();
-		System.out.print(b[2][0] + " | "+ b[2][1]+" | "+b[2][2]);
-		System.out.println();
+		for (int i = 0; i < 3; i++) {
+			System.out.print(b[i][0] + " | " + b[i][1] + " | " + b[i][2]);
+			System.out.println();
+		}
 	}
+
 }
